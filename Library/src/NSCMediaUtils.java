d7c2c5d57afae4aa460dd8d013e949c9b3c02595
Diego-Cano <64940291+Diego-Cano@users.noreply.github.com>
2023-11-08 18:17:01 -0800

    Update NSCMediaUtils.java

--- Library/src/NSCMediaUtils.java
+++ Library/src/NSCMediaUtils.java
@@ -4,6 +4,21 @@ public class NSCMediaUtils {
         System.out.println("Title: " + media.getTitle());
         System.out.println("NSC Collection: " + media.isNSCCollection());
 
+        if (media instanceof Book) {
+            Book book = (Book) media;
+            System.out.println("Author: " + book.getAuthor());
+            System.out.println("ISBN: " + book.getISBN());
+        } else if (media instanceof DVD) {
+            DVD dvd = (DVD) media;
+            System.out.println("Duration: " + dvd.getDuration() + " minutes");
+            System.out.println("Genre: " + dvd.getGenre());
+        } else if (media instanceof Magazine) {
+            Magazine magazine = (Magazine) media;
+            System.out.println("Issue Number: " + magazine.getIssueNumber());
+            System.out.println("Month: " + magazine.getMonth());
+        }
+
         System.out.println();
     }
 }
+
