6c2448719fe14032f8f47b134f48cd3ae54e2af1
Diego-Cano <64940291+Diego-Cano@users.noreply.github.com>
2023-12-11 23:01:09 -0800

    Update SongTest.java

--- Song Project Test 2/src/SongTest.java
+++ Song Project Test 2/src/SongTest.java
@@ -23,4 +23,47 @@ public class SongTest {
         // Check that toString doesn't start with "Song@"
         assertFalse(song.toString().startsWith("Song@"));
     }
+
+
+    @Test
+    public void testEquals() {
+        Song song1 = new Song("TestTrack", "TestArtist", "2023", "January", "1", "1000");
+        Song song2 = new Song("TestTrack", "TestArtist", "2023", "January", "1", "1000");
+        Song song3 = new Song("DifferentTrack", "DifferentArtist", "2023", "January", "1", "1000");
+
+        assertEquals(song1, song2);  // Check equality for two identical songs
+        assertNotEquals(song1, song3);  // Check inequality for two different songs
+    }
+
+    @Test
+    public void testEqualsWithNull() {
+        Song song1 = new Song("TestTrack", "TestArtist", "2023", "January", "1", "1000");
+        Song song2 = null;
+
+        assertNotEquals(song1, song2);  // Check inequality with null
+    }
+
+    @Test
+    public void testEqualsWithDifferentClass() {
+        Song song = new Song("TestTrack", "TestArtist", "2023", "January", "1", "1000");
+        String differentClassObject = "NotASong";
+
+        assertNotEquals(song, differentClassObject);  // Check inequality with a different class object
+    }
+
+    @Test
+    public void testGetReleasdDate() {
+        Song song = new Song("TestTrack", "TestArtist", "2023", "January", "1", "1000");
+        assertEquals("January/1/2023", song.getReleasdDate());
+    }
+    @Test
+    public void testCompareTo() {
+        Song song1 = new Song("TestTrack1", "TestArtist", "2023", "January", "1", "1000");
+        Song song2 = new Song("TestTrack2", "TestArtist", "2023", "January", "2", "1500");
+
+        assertTrue(song1.compareTo(song2) < 0);  // Check natural ordering by track name and release year
+    }
+
+
+
 }
